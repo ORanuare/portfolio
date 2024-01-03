22e7270d07dafe3e9a530275fdc2b1d544ec4240
Oscar Ranuare <ranuareo@gmail.com>
2024-01-03 00:38:09 -0300

    initial commit
    Project creation, basic setup ready to work with.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import { useDark, useToggle } from "@vueuse/core"
+    
+    const isDark = useDark()
+    const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+    <main class="bg-bones dark:bg-carbon h-screen w-full text-carbon dark:text-bones">
+        
+    </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        carbon: '#1f2424',
+        bones: '#f9f9f9',
+      },
+    },
+  },
+  plugins: [],
+}
+
