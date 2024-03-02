2acdc6193c28d8d6b0cd2869857d33fe5fc642a7
Oscar Ranuare <ranuareo@gmail.com>
2024-03-02 18:55:12 -0300

    🆕 Add Initial Structure to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
   import { RouterView } from 'vue-router'
+  import { useDark, useToggle } from '@vueuse/core'
+  import Profile from '@/components/Profile.vue'
+
+  const isDark = useDark()
+  const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <RouterView />
+  <main class="flex justify-center min-h-screen w-full bg-bones dark:bg-carbon text-carbon dark:text-bones">
+    <div>
+      <Profile />
+    </div>
+    <div class="flex justify-end w-[85%] min-screen">
+      <div class="w-[60%] py-24 overflow-auto">
+        <RouterView />
+      </div>
+    </div>
+  </main>
 </template>
