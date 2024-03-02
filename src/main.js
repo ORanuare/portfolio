c4a7c44ab1decd499b9c66aa500e51c1eb3617a2
Oscar Ranuare <ranuareo@gmail.com>
2024-03-02 19:52:45 -0300

    🆕 Add OhVueIcon Configuration & ViFileTypePython Icon

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { ViFileTypePython } from "oh-vue-icons/icons";
+
+addIcons( ViFileTypePython );
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.component("v-icon", OhVueIcon);
 app.mount('#app')
