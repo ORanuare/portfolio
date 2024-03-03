c6810be462038ab64a9d5c976a11ddb63cee1444
Oscar Ranuare <ranuareo@gmail.com>
2024-03-03 01:00:23 -0300

    🆕 Add About.vue component with developer introduction and contact information

---
+++ src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col gap-8 text-2xl font-medium text-carbon/70 dark:text-bones/50">
+        <h5>Hey there,</h5>
+
+        <p>I'm Oscar Ranuare, a passionate full stack web developer with a talent for turning ideas into digital reality. <b>With over five years of self-taught programming experience</b> under my belt, I've dived into the intricate world of web development, honing my skills across various technologies and frameworks.</p>
+
+        <p>From crafting seamless backend solutions with Python and Django to architecting lightning-fast APIs using FastAPI, I thrive on the challenge of <b>building robust, scalable systems</b> that deliver exceptional user experiences. On the frontend, I wield the power of JavaScript and TypeScript into web applications, leveraging frameworks like Vue.js to create <b>dynamic interfaces that captivate and engage users</b>.</p>
+
+        <p>But it's not just about the code for me – I'm <b>equally passionate about the aesthetics and user interface design</b>, embracing modern design principles with tools like Tailwind CSS to create sleek, responsive layouts.</p>
+
+        <p>When it comes to version control and collaboration, I'm no stranger to Git and GitHub, seamlessly <b>integrating into development workflows to streamline processes and facilitate teamwork</b>. And let's not forget about databases – I'm well-versed in PostgreSQL and some MongoDB, ensuring data is managed efficiently and securely across projects.</p>
+
+        <p>Whether it's deploying applications on Heroku, harnessing the power of cloud services like Google Cloud and AWS, or navigating the world of Linux, I'm <b>always eager to explore new technologies and push the boundaries</b> of what's possible in web development.</p>
+
+        <p>Let's collaborate and bring your vision to life – drop me a line, and let's make magic happen!</p>
+
+        <div class="flex gap-12 text-sm cursor-pointer border-b border-transparent hover:border-leaf w-max duration-200 ease-in-out text-leaf font-bold">
+            <a href="mailto:contact@oscarranuare.dev">Contact me through contact@oscarranuare.dev</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+b{
+    color: #10b981ca;
+}
+</style>
