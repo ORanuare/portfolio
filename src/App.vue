c9fc6b89a52995b1638be1aea240cda1a51d4a65
Oscar Ranuare <ranuareo@gmail.com>
2024-03-02 22:54:49 -0300

    🆕 Add Tabs Component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import Profile from '@/components/Profile.vue'
+  import Tabs from '@/components/Tabs.vue';
 </script>
 
 <template>
   <main class="flex justify-center min-h-screen w-full bg-bones dark:bg-carbon">
     <div class="flex w-[85%] justify-end py-24">
       <Profile />
-      <div class="flex w-[60%] min-h-screen overflow-auto">
+      <div class="flex flex-col gap-16 w-[60%] min-h-screen overflow-auto">
+          <Tabs />
           <RouterView />
       </div>
     </div>
