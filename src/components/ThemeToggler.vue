7e4a82ceffc600eabcb2a9c293ea147817e66bab
Oscar Ranuare <ranuareo@gmail.com>
2024-03-02 23:20:06 -0300

    🆕 Add ThemeToggler component

---
+++ src/components/ThemeToggler.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { useDark, useToggle } from '@vueuse/core'
+
+    const isDark = useDark()
+    const toggleDark = useToggle(isDark)
+
+    console.log(isDark.value)
+</script>
+
+<template>
+    <div class="flex items-center px-1 w-20 h-8 border-2 border-leaf rounded-full cursor-pointer relative" @click="toggleDark()">
+        <transition name="toggle" mode="out-in">
+            <span v-if="isDark" class="absolute w-5 h-5 rounded-full bg-leaf duration-200 ease"></span>
+            <span v-else class="absolute w-5 h-5 rounded-full bg-leaf duration-200 ease translate-x-12"></span>
+        </transition>
+    </div>
+</template>
