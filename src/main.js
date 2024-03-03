158eee8629572a7dc3aa9d0dc9b5bdd76f011682
Oscar Ranuare <ranuareo@gmail.com>
2024-03-03 01:58:44 -0300

    🆕 Add i18n configuration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import './assets/css/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
+// Locale imports
+import en from '@/locales/en.json'
+import es from '@/locales/es.json'
 
 import App from './App.vue'
 import router from './router'
@@ -11,10 +16,23 @@ import { ViFileTypePython, ViFileTypeDjango, SiFastapi, ViFileTypeJsOfficial, Vi
 
 addIcons( ViFileTypePython, ViFileTypeDjango, SiFastapi, ViFileTypeJsOfficial, ViFileTypeVue, ViFileTypeTailwind, ViFileTypeHtml, ViFileTypeCss, ViFileTypeAws, ViFileTypeGit, ViFileTypePgsql, ViFileTypeTypescriptOfficial, FcLinux, CoGithub, MdArrowdropdownRound, SiHeroku, FcGoogle, ViFileTypeNpm, IoLogoVercel, MdMinimizeOutlined, FaExternalLinkAlt, FaLock, IoLanguage );
 
+// i18n configuration
+const localeValue = document.cookie.split('locale=')[1] || 'en'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: localeValue,
+  messages: {
+    en: en,
+    es: es
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.component("v-icon", OhVueIcon);
 app.mount('#app')
