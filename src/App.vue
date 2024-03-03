87b2393bf656cbb0ce2622e7e33016123d54221f
Oscar Ranuare <ranuareo@gmail.com>
2024-03-02 22:11:41 -0300

    🛠️ Update width of main content area in App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <main class="flex justify-center min-h-screen w-full bg-bones dark:bg-carbon">
     <div class="flex w-[85%] justify-end py-24">
       <Profile />
-      <div class="flex w-[65%] min-h-screen overflow-auto">
+      <div class="flex w-[60%] min-h-screen overflow-auto">
           <RouterView />
       </div>
     </div>
