8dd7db514271456ec51a6efd4fc0660a7c90b97f
Oscar Ranuare <ranuareo@gmail.com>
2024-03-26 18:29:24 -0300

    🆕 Add Tooltip component to contact links on hover

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,13 @@
 <script setup>
+    import Tooltip from '@/components/Tooltip.vue';
     import contacts from '@/data/contact.json';
 </script>
 
 <template>
     <div class="flex gap-4">
-        <a v-for="(contact, index) in contacts" :key="index" :href="contact.url" :target="contact.target" class="cursor-pointer relative" :id="index">
+        <a v-for="(contact, index) in contacts" :key="index" :href="contact.url" :target="contact.target" class="cursor-pointer relative" :id="contact.name">
             <v-icon :name="contact.icon" scale="1.7" class="text-carbon dark:text-bones"/>
+            <Tooltip :tooltip="contact.name" :tooltipId="contact.name"/>
         </a>
     </div>
 </template>
