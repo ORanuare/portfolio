769a264e08f0a22653c4392c2a9547e80d773ee0
Oscar Ranuare <ranuareo@gmail.com>
2024-03-26 18:28:47 -0300

    🛠️ Update Tooltip position and alignment
    Now with right side position, it matches better the responsive design.

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -48,8 +48,8 @@
 
 <template>
     <transition name="fade" mode="out-in">
-        <div v-if="tooltip && showTooltip" class="absolute flex text-center items-center justify-center -top-10 min-w-12 text-bones bg-carbon dark:text-carbon dark:bg-bones rounded-lg" :class="customClass">
-            <v-icon name="md-arrowdropdown-round" scale="2" class="absolute -bottom-5 left-0 text-carbon dark:text-bones"/>
+        <div v-if="tooltip && showTooltip" class="absolute flex text-center items-center justify-center right-0 -top-11 min-w-12 text-bones bg-carbon dark:text-carbon dark:bg-bones rounded-lg" :class="customClass">
+            <v-icon name="md-arrowdropdown-round" scale="2" class="absolute -bottom-5 right-0 text-carbon dark:text-bones"/>
             <span class="text-sm p-2 font-medium select-none">{{ tooltip }}</span>
         </div>
     </transition>
