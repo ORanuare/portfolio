a75b536425d2a0a190c8345b3248b289e7aef1e5
Oscar Ranuare <ranuareo@gmail.com>
2024-03-26 18:24:23 -0300

    👷🏼 Refactor contact links in Contact.vue to use new json file

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,11 @@
+<script setup>
+    import contacts from '@/data/contact.json';
+</script>
+
 <template>
     <div class="flex gap-4">
-        <a href="https://github.com/ORanuare" target="_blank" class="cursor-pointer relative" id="github-contact">
-            <v-icon name="co-github" scale="1.7" class="text-carbon dark:text-bones"/>
-        </a>
-        <a href="mailto:contact@oscarranuare.dev" class="cursor-pointer relative" id="email-contact">
-            <v-icon name="hi-mail" scale="1.7" class="text-carbon dark:text-bones"/>
+        <a v-for="(contact, index) in contacts" :key="index" :href="contact.url" :target="contact.target" class="cursor-pointer relative" :id="index">
+            <v-icon :name="contact.icon" scale="1.7" class="text-carbon dark:text-bones"/>
         </a>
     </div>
 </template>
