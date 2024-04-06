2bec78a278560e3a24c09438c1ae9eaf8e1b68f7
Oscar Ranuare <ranuareo@gmail.com>
2024-04-05 23:32:27 -0300

    🆕 Add copy functionality to Contact.vue social links

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,13 +1,32 @@
 <script setup>
+    import { ref } from 'vue';
     import Tooltip from '@/components/Tooltip.vue';
     import contacts from '@/data/contact.json';
+
+    const copying = ref(false);
+
+    const copyToClipboard = async(text) => {
+        copying.value = true;
+
+        navigator.clipboard.writeText(text);
+        await new Promise((resolve) => setTimeout(resolve, 1800));
+
+        copying.value = false;
+    }
 </script>
 
 <template>
     <div class="flex gap-4">
-        <a v-for="(contact, index) in contacts" :key="index" :href="contact.url" :target="contact.target" class="cursor-pointer relative" :id="contact.name">
-            <v-icon :name="contact.icon" scale="1.7" class="text-carbon dark:text-bones"/>
-            <Tooltip :tooltip="contact.name" :tooltipId="contact.name"/>
-        </a>
+        <div v-for="(contact, index) in contacts" :key="index" class="cursor-pointer relative" :id="contact.name">
+            
+            <a v-if="contact.type === 'url'" :href="contact.url" :target="contact.target">
+                <v-icon :name="contact.icon" scale="1.7" class="text-carbon dark:text-bones"/>
+                <Tooltip :tooltip="contact.name" :tooltipId="contact.name"/>
+            </a>
+            <span v-else-if="contact.type === 'copy'" @click="copyToClipboard(contact.value)">
+                <v-icon :name="contact.icon" scale="1.7" class="text-carbon dark:text-bones"/>
+                <Tooltip :tooltip="!copying ? contact.name : '¡Has copiado el '+contact.name+' al portapapeles!'" :tooltipId="contact.name"/>
+            </span>      
+        </div>
     </div>
 </template>
